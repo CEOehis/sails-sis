a5b36cc66ae235371f53b555916a2bfea84c6029
ceoehis <ceo.ehis@outlook.com>
2017-10-27 13:58:47 +0100

    add api for student resource

---
+++ api/controllers/StudentResourceController.js
@@ -0,0 +1,11 @@
+/**
+ * StudentResourceController
+ *
+ * @description :: Server-side logic for managing Studentresources
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
