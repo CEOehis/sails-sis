eedae473a9869f4c20604f51e3608bdfd60110ff
ceoehis <ceo.ehis@outlook.com>
2017-10-28 05:15:47 +0100

    add bootstrap

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -29,7 +29,7 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+  'js/vendor/jquery-3.2.1.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
