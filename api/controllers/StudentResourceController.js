3772b322ee7abc21dc6e3e39e1a1ba742e8f0ce4
ceoehis <ceo.ehis@outlook.com>
2017-10-29 04:53:08 +0100

    define controller for resource actions

--- api/controllers/StudentResourceController.js
+++ api/controllers/StudentResourceController.js
@@ -6,6 +6,69 @@
  */
 
 module.exports = {
-	
-};
+	  // REST create action
+	create: function(req, res, next) {
+
+	  var params = req.params.all();
+
+	  StudentResource.create(params, function(err, resource) {
+
+      if (err) return next(err);
+
+      res.status(201);
+
+      res.json(resource);
+
+	  });
+	},
+
+	// REST read/get action
+	find: function (req, res, next) {
+		var id = req.param('id');
+		var idShortCut = isShortCut(id);
+
+		if(idShortCut) {
+			return next();
+		}
+
+		if (id) {
+			StudentResource.findOne(id, function(err, resource) {
+				if(resource === undefined) return res.notFound();
+
+				if(err) return next(err);
+
+				res.json(resource)
+			})
+		} else {
+			var where = req.param('where');
+
+			if(_.isString(where)) {
+				where = JSON.parse(where);
+			}
+
+			var options = {
+				limit: req.param('limit') || undefined,
+				skip: req.param('skip') || undefined,
+				sort: req.param('sort') || undefined,
+				where: where || undefined
+			};
+
+			console.log("This is the options", options);
+
+			StudentResource.find(options, function(err, resource) {
+				if(resource === undefined) return res.notFound();
+
+				if(err) return next(err);
+
+				res.json(resource);
+			});
+		}
+
+		function isShortCut(id) {
+			if(id === 'find' || id === 'update' || id === 'create' || id === 'destroy') {
+				return true;
+			}
+		}
+	},
+}
 
